eada7abed5c418fe4884261a7863b321c692d2ff
Neneth Robledo <neneth.robledo@rezdy.com>
2020-11-27 17:50:17 +1100

    Initial codebase

---
+++ src/test/java/com/sample/mnf/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.mnf.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
